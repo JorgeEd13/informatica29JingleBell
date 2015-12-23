8cb531d51c24c4cacdf49e5016dcf01ccc51f8ec
Joao Gabriel Gouveia De Souza Brito <gabriel.gouveia@live.com>
2015-12-23 18:47:06 -0300

    Correção de identação e função void.

--- ex29.c
+++ ex29.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int tabuleiro[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
 int modo_jogo, dificuldade;
 
 int nivelfacil(int tab[3][3], int vez){
@@ -14,7 +13,8 @@ int niveldificil(int tab[3][3], int vez){
     return 0;
 }
 
-int verificarJogo(int tab[3][3]){
+int verificarJogo(int tab[3][3])
+{
     //retorna 0 caso o jogo não tenha terminado
     //retorna 1 caso o jogo tenha terminado e o jogador 1 ganhou
     //retorna 2 caso o jogo tenha terminado e o jogador 2 ganhou
@@ -22,21 +22,25 @@ int verificarJogo(int tab[3][3]){
     return 0;
 }
 
-void imprimeTabuleiro(int tab[3][3]){
+void imprimeTabuleiro(int tab[3][3])
+{
     //Imprimir formatado o tabuleiro
 }
 
 
 
-int main(){
-
-    do{
+int main(void)
+{
+    int tabuleiro[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+    do
+    {
         printf("Digite o modo de jogo\n1- Humano contra Humano\n2- Computador contra Computador\n3- Humano contra Computador:\n");
         scanf("%d", &modo_jogo);
     }while(modo_jogo<1 && modo_jogo>3);
 
 
-    if(modo_jogo ==2 || modo_jogo ==3){
+    if(modo_jogo ==2 || modo_jogo ==3)
+    {
 
         do{
             printf("Digite a dificuldade\n1-facil\n2-medio\n3-dificil:\n");
@@ -44,7 +48,8 @@ int main(){
         }while(dificuldade<1 && dificuldade>3);
 
     }
-    while(verificarJogo(tabuleiro) == 0){
+    while(verificarJogo(tabuleiro) == 0)
+    {
 
     }
 
