dbc6b5a3dba833466d22f7a440a9b41295542e21
Jorge Edson Ribeiro da Silva Neto <jorge.ed.ribeiro00@gmail.com>
2015-12-28 23:25:35 -0300

    alteracoes finais

--- ex29.c
+++ ex29.c
@@ -185,15 +185,15 @@ if(jogada!=9)
 
 
     srand(time(NULL));
-    int r = rand()%5; //sorteia uma das 4 diagonais
-    int vetorDiagonais[5]={0,2,4,6,8};//vetor com a posição das diagonais e do centro
+    int r = rand()%5; /*sorteia uma das 4 diagonais*/
+    int vetorDiagonais[5]={0,2,4,6,8};/*vetor com a posição das diagonais e do centro*/
 int cont=1;
-while(verificaJogadaValida(tab, vetorDiagonais[r])==0){ //caso a posição ja tenha sido escolhida, vai decrementando ate achar uma posicao
+while(verificaJogadaValida(tab, vetorDiagonais[r])==0){ /*caso a posição ja tenha sido escolhida, vai decrementando ate achar uma posicao*/
     r--;
     cont++;
-    if(r==-1) //caso extrapole a primeira posição, vai pra ultima pra começar a busca
+    if(r==-1) /*caso extrapole a primeira posição, vai pra ultima pra começar a busca*/
         r=4;
-    if(cont>=5)// se ele tentou jogar nas diagonais e no centro e não funcionou, joga aleatorio
+    if(cont>=5)/*se ele tentou jogar nas diagonais e no centro e não funcionou, joga aleatorio*/
         return nivelfacil(tab, vez);
 }
 
